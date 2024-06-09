7136e39bd687653228389ece9e07c02db6bdffb2
Chunhui Ren <chunhui@192.168.10.2>
2024-06-09 23:02:40 +0800

    ci: add husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,73 +1,74 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
   ],
   rules: {
-    semi: ["error", "never"],
-    quotes: ["error", "single"],
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
-    "vue/custom-event-name-casing": ["error", "camelCase"],
-    "vue/define-macros-order": [
-      "error",
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'linebreak-style': ['error', 'unix'],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/custom-event-name-casing': ['error', 'camelCase'],
+    'vue/define-macros-order': [
+      'error',
       {
-        order: ["defineProps", "defineEmits"],
+        order: ['defineProps', 'defineEmits'],
       },
     ],
-    "vue/attribute-hyphenation": ["error", "always"],
-    "vue/first-attribute-linebreak": [
-      "error",
+    'vue/attribute-hyphenation': ['error', 'always'],
+    'vue/first-attribute-linebreak': [
+      'error',
       {
-        singleline: "ignore",
-        multiline: "below",
+        singleline: 'ignore',
+        multiline: 'below',
       },
     ],
-    "vue/html-closing-bracket-newline": [
-      "error",
+    'vue/html-closing-bracket-newline': [
+      'error',
       {
-        singleline: "never",
-        multiline: "never",
+        singleline: 'never',
+        multiline: 'never',
       },
     ],
-    "vue/html-closing-bracket-spacing": [
-      "error",
+    'vue/html-closing-bracket-spacing': [
+      'error',
       {
-        startTag: "never",
-        endTag: "never",
-        selfClosingTag: "always",
+        startTag: 'never',
+        endTag: 'never',
+        selfClosingTag: 'always',
       },
     ],
-    "vue/html-indent": "error",
-    "vue/html-quotes": ["error", "double"],
-    "vue/max-attributes-per-line": [
-      "error",
+    'vue/html-indent': 'error',
+    'vue/html-quotes': ['error', 'double'],
+    'vue/max-attributes-per-line': [
+      'error',
       {
         singleline: { max: 2 },
         multiline: { max: 1 },
       },
     ],
-    "vue/mustache-interpolation-spacing": ["error", "always"],
-    "vue/prop-name-casing": ["error", "camelCase"],
-    "vue/multi-word-component-names": "off",
-    "vue/no-multi-spaces": "error",
-    "vue/no-textarea-mustache": "error",
-    "vue/no-use-v-if-with-v-for": "error",
-    "vue/no-watch-after-await": "error",
-    "vue/block-lang": [
-      "error",
+    'vue/mustache-interpolation-spacing': ['error', 'always'],
+    'vue/prop-name-casing': ['error', 'camelCase'],
+    'vue/multi-word-component-names': 'off',
+    'vue/no-multi-spaces': 'error',
+    'vue/no-textarea-mustache': 'error',
+    'vue/no-use-v-if-with-v-for': 'error',
+    'vue/no-watch-after-await': 'error',
+    'vue/block-lang': [
+      'error',
       {
         script: {
-          lang: "ts",
+          lang: 'ts',
         },
       },
     ],
   },
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
-};
+}
--- README.md
+++ README.md
@@ -1,39 +1,12 @@
-# vue-admin
+## 预配置
 
-This template should help get you started developing with Vue 3 in Vite.
+### UI 框架
 
-## Recommended IDE Setup
+- tailwindcss
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+### 插件
 
-## Type Support for `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
-## Project Setup
-
-```sh
-pnpm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-pnpm dev
-```
-
-### Type-Check, Compile and Minify for Production
-
-```sh
-pnpm build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-pnpm lint
-```
+- unplugin-vue-components
+- unplugin-auth-import
+- VueUse
+- vite-plugin-vue-devtools
